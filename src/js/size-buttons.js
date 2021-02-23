ab42177c2a3f34ed7416a32923f0e890ac3d49ce
Ismael Ibanez <ismaelibanezdev@gmail.com>
2021-02-23 21:08:36 +0000

    addeventlistener

--- src/js/size-buttons.js
+++ src/js/size-buttons.js
@@ -7,3 +7,6 @@ function sizeDown(){
     let resultado = parseInt(document.querySelector('.size-number').textContent) - 1 ;
     document.querySelector('.size-number').textContent = String(resultado);
 }
+
+document.querySelector(".size-up").addEventListener("click", sizeUp);
+document.querySelector(".size-down").addEventListener("click", sizeDown);
